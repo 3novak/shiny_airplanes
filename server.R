eed399252fc5e3fd4dd7a715061e672ef3c23184
Ethan Novak <ewnovak@Ethans-Air.fios-router.home>
2016-12-17 12:24:23 -0500

    adjusted plot settings

--- server.R
+++ server.R
@@ -7,28 +7,47 @@ library(scales)
 library(rgeos)
 
 shinyServer(function(input, output){
+  
+  ranges <- reactiveValues(x = NULL, y = NULL)
+  
   output$mapPlot <- renderPlot({
 
+    # zoom controls
+    observeEvent(input$zoom_dblclick, {
+      brush <- input$zoom_brush
+      if (!is.null(brush)) {
+        ranges$x <- c(brush$xmin, brush$xmax)
+        ranges$y <- c(brush$ymin, brush$ymax)
+        
+      } else {
+        ranges$x <- NULL
+        ranges$y <- NULL
+      }
+    })
+    
     # modifications to the data from the inputs through UI.R
-    data <- data[(altitude > input$alt_cut[1]) & (altitude < input$alt_cut[2]),]
-    data <- data[qsec > input$time_cut_min,]
-    data <- data[qsec_local > input$time_min_local,]
+    air_data <- air_data[(altitude > input$alt_cut[1]) & (altitude < input$alt_cut[2]),]
+    air_data <- air_data[qsec > input$time_cut_min,]
+    air_data <- air_data[qsec_local > input$time_min_local,]
     if (input$choose_color == 'grad'){
-      data[, colors := grad]
+      air_data[, colors := grad]
     } else {
-      data[, colors := input$choose_color] 
+      air_data[, colors := input$choose_color]
     }
     
+    stateData <- stateLines500k
+
     # define the plot
     # use geom_path() for shapes with no fill
-    ggplot() + geom_polygon(data = stateLines, aes(x = long, y = lat),
+    ggplot() + geom_polygon(data = stateData, 
+                            aes(x = long, y = lat),
                             color = 'black',
-                            size = 0.25, 
-                            group = stateLines[, group],
+                            group = stateData[, group],
+                            size = 0.2, 
                             fill = 'white') +
-               coord_map() +
-               geom_point(data = data, colour = data[, colors],
-                          aes(x = data[, long], y = data[, lat]))
+               labs(x = 'Latitude', y = 'Longitude') +
+               coord_map(xlim = ranges$x, ylim = ranges$y) +
+               geom_point(data = air_data, colour = air_data[, colors],
+                          aes(x = long, y = lat))
   })
 })
-
