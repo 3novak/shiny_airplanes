3cd200587c21d963af58716fd0986b00a2ade6f7
Ethan Novak <ewnovak@Ethans-Air.fios-router.home>
2016-12-22 20:08:35 -0500

    transitioned to leaflet package

--- server.R
+++ server.R
@@ -5,49 +5,47 @@ library(ggmap)
 library(rgdal)
 library(scales)
 library(rgeos)
+library(leaflet)
 
 shinyServer(function(input, output){
   
-  ranges <- reactiveValues(x = NULL, y = NULL)
+  stateData <- stateLines500k
   
-  output$mapPlot <- renderPlot({
-
-    # zoom controls
-    observeEvent(input$zoom_dblclick, {
-      brush <- input$zoom_brush
-      if (!is.null(brush)) {
-        ranges$x <- c(brush$xmin, brush$xmax)
-        ranges$y <- c(brush$ymin, brush$ymax)
-        
-      } else {
-        ranges$x <- NULL
-        ranges$y <- NULL
+  subsetData <- reactive({
+      new_data <- air_data[altitude > input$alt_cut[1] &
+                           altitude < input$alt_cut[2] &
+                           qsec >= input$time_cut_min & 
+                           qsec_local >= input$time_min_local, ]
+      
+      if (input$choose_color == 'red'){
+        new_data[, colors := 'red']
+      } else if (input$choose_color == 'navy'){
+        new_data[, colors := 'navy']
+      } else if (input$choose_color == 'grad'){
+        new_data[, colors := grad]
       }
-    })
-    
-    # modifications to the data from the inputs through UI.R
-    air_data <- air_data[(altitude > input$alt_cut[1]) & (altitude < input$alt_cut[2]),]
-    air_data <- air_data[qsec > input$time_cut_min,]
-    air_data <- air_data[qsec_local > input$time_min_local,]
-    if (input$choose_color == 'grad'){
-      air_data[, colors := grad]
-    } else {
-      air_data[, colors := input$choose_color]
-    }
-    
-    stateData <- stateLines500k
-
-    # define the plot
-    # use geom_path() for shapes with no fill
-    ggplot() + geom_polygon(data = stateData, 
-                            aes(x = long, y = lat),
-                            color = 'black',
-                            group = stateData[, group],
-                            size = 0.2, 
-                            fill = 'white') +
-               labs(x = 'Latitude', y = 'Longitude') +
-               coord_map(xlim = ranges$x, ylim = ranges$y) +
-               geom_point(data = air_data, colour = air_data[, colors],
-                          aes(x = long, y = lat))
+      return(new_data)
+  })
+  
+  output$mapPlot <- renderLeaflet({
+    leaflet() %>%
+      addPolygons(data = stateData,
+                  color = '#9babbc',
+                  weight = 2,
+                  fillColor = '#abbbcc',
+                  fillOpacity = 0.8,
+                  smoothFactor = .2)
+  })
+  
+  observe({
+    leafletProxy('mapPlot') %>%
+      clearGroup('A') %>%
+      addCircles(data = subsetData(),
+                 group = 'A',
+                 lng = ~long,
+                 lat = ~lat,
+                 radius = 2,
+                 weight = 2,
+                 color = ~colors)
   })
 })
