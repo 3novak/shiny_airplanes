caad81b6cd108dd7aa4bd09b5f20f8b96b1c0bc0
Ethan Novak <ewnovak@Ethans-Air.fios-router.home>
2016-12-22 20:12:42 -0500

    added to the TODO list and cleaned up the order

--- global.R
+++ global.R
@@ -1,3 +1,20 @@
+# data for plotting is loaded here to be visible to UI.R and server.R
+
+# TODO list
+# TODO: select data: county level, etc.
+# TODO: path indicators: add triangles, pictures, sizes, etc.
+# TODO: add a longer flight
+# TODO: add a pan cursor
+# TODO: scroll zoom
+# TODO: select on an origin airport
+# TODO: select on a destination airport
+# TODO: select on flight duration
+# TODO: select on maximum altitude attained
+# TODO: select on distance (as the crow flies) traveled
+# TODO: show desc stats
+# TODO: add a color-coded legend for the speed
+# TODO: tighten up the left panel
+
 # shapefile downloaded from http://www.census.gov/geo/maps-data/data/cbf/cbf_state.html
 #stateLines5m <- readOGR(dsn = '/Users/ewnovak/Documents/projects/shiny_airplanes/cb_2015_us_state_5m',
 #                      layer = 'cb_2015_us_state_5m')
@@ -6,20 +23,21 @@
 
 stateLines500k <- readOGR(dsn = '/Users/ewnovak/Documents/projects/shiny_airplanes/cb_2015_us_state_5m',
                           layer = 'cb_2015_us_state_5m')
-stateLines500k <- stateLines[-c(3,23,33,34,41,42,43),]  # remove OCONUS locations
-stateLines500k <- as.data.table(fortify(stateLines, region = 'GEOID'))
-
-
-# TODO: select data
-
+stateLines500k <- stateLines500k[-c(3,23,33,34,41,42,43),]  # remove OCONUS locations
+#stateLines500k <- as.data.table(fortify(stateLines500k, region = 'GEOID'))
 
 # loading and preprocessing data for the flights
 setwd('~/Documents/projects/shiny_airplanes')
-air_data <- as.data.table(read.csv('data.txt', sep = '|'))
+air_data <- as.data.table(read.csv('~/Documents/projects/shiny_airplanes/data.txt', sep = '|'))
+air_data <- air_data[lat != 0 & long != 0,]
 air_data[, datetime := as.POSIXct(paste(date, time))]
+
+# sort on speed in order to apply the palette
 setkey(air_data, mph)
-air_data[, grad := colorRampPalette(c('red', 'yellow', 'green'))(nrow(air_data))]
+air_data[, grad := colorRampPalette(c('blue', 'orange', 'red'))(nrow(air_data))]
 air_data[, colors := grad]
+
+# sort on time in order to assign time quantiles
 setkey(air_data, datetime)
 air_data[, seconds := as.integer(datetime)]
 air_data[, qsec := as.numeric(cut(seconds, quantile(seconds, 0:10/10), 
@@ -27,4 +45,3 @@ air_data[, qsec := as.numeric(cut(seconds, quantile(seconds, 0:10/10),
 air_data[, qsec_local := as.numeric(cut(seconds, quantile(seconds, 0:10/10), 
                                     labels = 1:10, include.lowest = TRUE)),
          by = .(aircraft, origin, destination)]
-
