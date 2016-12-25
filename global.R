be23f7183f3be1abd08fed4054dace101f6072ca
Ethan Novak <ewnovak@Ethans-MacBook-Air.local>
2016-12-25 15:31:57 -0500

    Added the descriptive stats tab and an initial ggplot display.

--- global.R
+++ global.R
@@ -1,19 +1,14 @@
 # data for plotting is loaded here to be visible to UI.R and server.R
 
-# TODO list
+
 # TODO: select data: county level, etc.
 # TODO: path indicators: add triangles, pictures, sizes, etc.
 # TODO: add a longer flight
-# TODO: add a pan cursor
-# TODO: scroll zoom
-# TODO: select on an origin airport
-# TODO: select on a destination airport
 # TODO: select on flight duration
-# TODO: select on maximum altitude attained
 # TODO: select on distance (as the crow flies) traveled
-# TODO: show desc stats
-# TODO: add a color-coded legend for the speed
-# TODO: tighten up the left panel
+# TODO: add airport markers
+# TODO: perform stats by dest/origin airport
+
 
 # shapefile downloaded from http://www.census.gov/geo/maps-data/data/cbf/cbf_state.html
 #stateLines5m <- readOGR(dsn = '/Users/ewnovak/Documents/projects/shiny_airplanes/cb_2015_us_state_5m',
@@ -29,13 +24,21 @@ stateLines500k <- stateLines500k[-c(3,23,33,34,41,42,43),]  # remove OCONUS loca
 # loading and preprocessing data for the flights
 setwd('~/Documents/projects/shiny_airplanes')
 air_data <- as.data.table(read.csv('~/Documents/projects/shiny_airplanes/data.txt', sep = '|'))
+air_data <- air_data[!is.null(mph),]
 air_data <- air_data[lat != 0 & long != 0,]
 air_data[, datetime := as.POSIXct(paste(date, time))]
 
 # sort on speed in order to apply the palette
 setkey(air_data, mph)
-air_data[, grad := colorRampPalette(c('blue', 'orange', 'red'))(nrow(air_data))]
+colorPal <- colorRampPalette(c('blue', 'orange', 'red'))
+air_data[, grad := colorPal(nrow(air_data))]
 air_data[, colors := grad]
+
+num_bins <- 10
+speed_cuts <- levels(cut(air_data[, mph], breaks = num_bins))
+qpal <- colorQuantile(colorPal, air_data[, mph], n=5)
+
+pal <- colorNumeric(palette = 'YlGnBu', domain = air_data[, mph])
 
 # sort on time in order to assign time quantiles
 setkey(air_data, datetime)
@@ -45,3 +48,6 @@ air_data[, qsec := as.numeric(cut(seconds, quantile(seconds, 0:10/10),
 air_data[, qsec_local := as.numeric(cut(seconds, quantile(seconds, 0:10/10), 
                                     labels = 1:10, include.lowest = TRUE)),
          by = .(aircraft, origin, destination)]
+air_data[, flight_id := factor(rleidv(air_data, cols = c('aircraft', 'origin', 'destination')))]
+air_data[, bin_mph := as.numeric(cut(mph, breaks = c(-1, 100, 200, 300, 400, 10000)))]
+air_data[, qsec_factor := as.factor(qsec_local)]
--- server.R
+++ server.R
@@ -3,6 +3,8 @@ library(data.table)
 library(rgdal)
 library(rgeos)
 library(leaflet)
+library(ggplot2)
+library(scales)
 
 shinyServer(function(input, output){
   
@@ -11,7 +13,8 @@ shinyServer(function(input, output){
   subsetData <- reactive({
       new_data <- air_data[altitude >= input$alt_cut[1] &
                            altitude <= input$alt_cut[2] &
-                           qsec >= input$time_cut_min & 
+                           datetime >= input$time_range[1] &
+                           datetime <= input$time_range[2] &
                            qsec_local >= input$time_min_local &
                            origin %in% input$origin &
                            destination %in% input$dest, ]
@@ -65,4 +68,20 @@ shinyServer(function(input, output){
       proxy %>% addLegend(position = 'bottomright', pal = pal, values = ~mph) 
     }
   })
+  
+  # desc stats section
+  output$statsPlot <- renderPlot({
+  ggplot(data = subsetData(), aes(x = subsetData()$qsec_factor, y = subsetData()$altitude)) + 
+    geom_point(aes(size = subsetData()$bin_mph, color = subsetData()$flight_id), alpha = .4) +
+    labs(title = '\nAircraft altitude over the course of a flight', 
+         x = 'Flight time decile', 
+         y = 'Altitude (ft.)', 
+         color = 'Flight', 
+         size = 'Speed quintile') +
+    scale_color_manual(values = c('red', 'blue')) +
+    # TODO: add labels to the legend so it looks better
+    # c('[0, 100)', '[100, 200)', '[200, 300)', '[300, 400)', '[400, inf)') +
+    scale_y_continuous(labels = comma)
+  })
+
 })
