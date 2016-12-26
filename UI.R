ecf871369444cb3aae33e2e25e1a824259e86547
Ethan Novak <ethan.novak@gmail.com>
2016-12-26 15:19:37 -0500

    reformatted the panel layout

--- UI.R
+++ UI.R
@@ -3,7 +3,7 @@ shinyUI(fluidPage(
   titlePanel('Aircraft Flights'),
     sidebarPanel(
       tabsetPanel(
-        tabPanel('Plot',
+        tabPanel('Flight',
           sliderInput('alt_cut',
                       'Altitude range:',
                       min = 0,
@@ -11,6 +11,27 @@ shinyUI(fluidPage(
                       value = c(0, 40000),
                       step = 1000),
           tags$hr(),
+          selectInput('origin',
+                      'Filter on origin:',
+                      choices = air_data[, unique(origin)],
+                      selected = air_data[, unique(origin)],
+                      multiple = TRUE,
+                      selectize = FALSE),
+          selectInput('dest',
+                      'Filter on destination:',
+                      choices = air_data[, unique(destination)],
+                      selected = air_data[, unique(destination)],
+                      multiple = TRUE,
+                      selectize = FALSE),
+          tags$hr(),
+          selectInput('flight',
+                      'Choose a flight:',
+                      choices = air_data[, unique(flight_id)],
+                      selected = air_data[, unique(flight_id)],
+                      multiple = TRUE,
+                      selectize = FALSE)
+        ),
+        tabPanel('Time',
           sliderInput('time_range',
                       'Time range:',
                       min = min(air_data$datetime),
@@ -23,34 +44,21 @@ shinyUI(fluidPage(
                       min = 1,
                       max = max(air_data$qsec_local),
                       value = 1,
-                      step = 1),
-          tags$hr(),
-          selectInput('choose_color',
-                      'Select path color:',
-                      choices = list('red' = 'red', 'navy' = 'navy', 'gradient' = 'grad'),
-                      selected = 'grad'),
-          checkboxInput('legend', label = 'Display Legend', value = TRUE),
-          tags$hr(),
-          selectInput('origin',
-                      'Filter on origin',
-                      choices = air_data[, unique(origin)],
-                      selected = air_data[, unique(origin)],
-                      multiple = TRUE,
-                      selectize = FALSE),
-          selectInput('dest',
-                      'Filter on destination',
-                      choices = air_data[, unique(destination)],
-                      selected = air_data[, unique(destination)],
-                      multiple = TRUE,
-                      selectize = FALSE)
-          ),  # end of first panel
-        tabPanel('Stats',
-          selectInput('desc1',
-                      'do some stats',
-                      choices = c('choice 1', 'beef', 'broccoli'),
-                      selected = 'beef',
-                      multiple = FALSE,
-                      selectize = TRUE)
+                      step = 1)
+        ),
+        tabPanel('Display',
+           selectInput('choose_color',
+                       'Select path color:',
+                       choices = list('red' = 'red', 'navy' = 'navy', 'gradient' = 'grad'),
+                       selected = 'grad'),
+           checkboxInput('legend', label = 'Display Legend', value = TRUE),  # have the default depend on the selection of 'choose_color'
+           tags$hr(),
+           selectInput('agg_level',
+                       'Choose aggregation level:',
+                       choices = list('deciles across flights' = 'bydec',
+                                      'decile within flights' = 'bydecbyflight'),
+                       selected = 'bydecbyflight',
+                       multiple = FALSE)
         )
     ),
     width = 3),
@@ -59,8 +67,9 @@ shinyUI(fluidPage(
         tabPanel('Main display',
           leafletOutput('mapPlot')
         ),
-        tabPanel('Descriptive stats',
-          plotOutput('statsPlot')
+        tabPanel('Altitude graph',
+          plotOutput('statsPlot'),
+          p(class = 'text-muted', '\n\nplot only includes data subject to the conditions in the Flight, Time, and Display tabs')
         )
       )
     )
